dc16fe6be0f94c45f303c81c279acc464210d76c
Mads Horn <madshorn@operamail.com>
2019-11-01 13:46:25 +0100

    Add skeleton to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+extern crate rand;
+
+use nannou::prelude::*;
+use std::thread::sleep;
+use std::time;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+
+    nannou::app(model)
+        .update(update)
+        .run()
+}
+
+struct Model {
+    _window: WindowId,
+}
+
+fn model(app: &App) -> Model {
+
+    let _window = app
+    .new_window()
+    .with_dimensions(700, 700)
+    .with_title("Simple Neural Network")
+    .view(view)
+    .build()
+    .unwrap();
+
+    Model {
+        _window
+    }
+}
+
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+
+    //if model.time % 512 < 16 {
+        // println!("time: {:?} cost: {:?}", model.time, find_cost(model));
+    if 5 > 4 {
+        println!("time: ");
+
+        sleep(time::Duration::new(0, 500000000)); // sec, nano sec
+    }
+}
+
+
+fn view(app: &App, model: &Model, frame: &Frame) {
+
+    let draw = app.draw();
+
+    draw_results(model, &draw);
+
+    draw.to_frame(app, frame).unwrap();
+}
+
+fn draw_results(model: &Model, draw: &nannou::app::Draw) {
+
+    let sq_col = 255.0; // model.handwrittendigitthingy
+
+    draw.quad().x_y(10.0, 10.0).color(rgb(sq_col, sq_col, sq_col));
 }
