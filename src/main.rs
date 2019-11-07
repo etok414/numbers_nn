d6c8299eff6805dd2c22fce39757910b22cf3d93
Toke Horn Brorholt <etok414@gmail.com>
2019-11-07 12:55:14 +0100

    added unpack_images and some support to unpacking.rs, removed the commented out old functions from nodes_layers.rs and fixed some errors in it, and mostly implemented those in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,10 @@ fn main() {
 
 struct Model {
     _window: WindowId,
+    network: nodes_layers::Network,
+    training_images: Vec<Vec<f32>>,
+    training_labels: Vec<Vec<f32>>,
+    counter: usize,
 }
 
 fn model(app: &App) -> Model {
@@ -26,8 +30,18 @@ fn model(app: &App) -> Model {
     .build()
     .unwrap();
 
+    let network = nodes_layers::Network::new(vec![28*28, 16, 16, 9], 0.5);
+    let training_images = unpacking::unpack_images(r".\datas\train-images.idx3-ubyte");
+    let training_labels = unpacking::unpack_labels(r".\datas\train-labels.idx1-ubyte");
+    let training_labels = unpacking::turn_to_result(training_labels);
+    let counter = 0;
+
     Model {
-        _window
+        _window,
+        network,
+        training_images,
+        training_labels,
+        counter,
     }
 }
 
@@ -36,6 +50,8 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
 
     //if model.time % 512 < 16 {
         // println!("time: {:?} cost: {:?}", model.time, find_cost(model));
+    model.network.find_make_adjust(&model.training_images[model.counter], &model.training_labels[model.counter]);
+    model.counter += 1;
     if 5 > 4 {
         println!("time: ");
 
@@ -55,16 +71,18 @@ fn view(app: &App, model: &Model, frame: &Frame) {
 
 fn draw_results(model: &Model, draw: &nannou::app::Draw) {
 
-    let sq_col = 255.0; // model.input
-    let cir_col = 255.0; // model.output
+    // let sq_col = 255.0; // model.input
+    // let cir_col = 255.0; // model.output
 
-    for y in 0..27 {
-        for x in 0..27 {
+    for y in (0..28).rev() {
+        for x in 0..28 {
+            let sq_col = model.training_images[model.counter][y*28 + x] * 255.0;
             draw.rect().x_y(2.0 * x as f32 - 200.0, 2.0 * y as f32 + 50.0).w_h(2.0, 2.0)
                             .color(rgb(sq_col, sq_col, sq_col));
         }
     }
     for y in 0..9 {
+        let cir_col = model.training_labels[model.counter][y] * 255.0;
         draw.ellipse().x_y(200.0, 10.0 * y as f32).radius(6.0)
                            .color(rgb(cir_col, cir_col, cir_col));
     }
--- src/unpacking.rs
+++ src/unpacking.rs
@@ -6,3 +6,37 @@ pub fn unpack_labels(filename: &str) -> Vec<u8> {
         [8..].to_vec();
     content
 }
+
+pub fn turn_to_result(input: Vec<u8>) -> Vec<Vec<f32>> {
+    let mut output = Vec::new();
+    for number in input {
+        let mut element = vec![0.0;10];
+        element[number as usize] = 1.0;
+        output.push(element);
+    }
+    output
+}
+
+pub fn unpack_images(filename: &str) -> Vec<Vec<f32>> {
+        let raw_content = fs::read(filename)
+            .expect("Something went wrong while reading images")
+            [16..].to_vec();
+        let raw_content = turn_to_float(raw_content);
+        let content_as_iter = raw_content.chunks_exact(28*28);
+        if content_as_iter.remainder().len() > 0 {
+            panic!("An image was only partially formed. It had only {:?} elements, when it should have {:?}", content_as_iter.remainder().len(), 28*28)
+        }
+        let mut content = Vec::new();
+        for chonk in content_as_iter {
+            content.push(chonk.to_vec());
+        }
+        content
+}
+
+pub fn turn_to_float(input: Vec<u8>) -> Vec<f32> {
+    let mut output = Vec::new();
+    for number in input {
+        output.push(number as f32 / 255.0)
+    }
+    output
+}
